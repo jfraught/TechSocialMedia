8fc80a44258d87b25878bcb9c1044063a04ac7f6
Jordan Fraughton <fraughtdawg@gmail.com>
2024-06-25 08:50:54 -0600

    Initial Commit

---
+++ TechSocialMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechSocialMedia
+//
+//  Created by Jordan Fraughton on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
