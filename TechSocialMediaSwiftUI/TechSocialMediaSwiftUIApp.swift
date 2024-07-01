7c99ad15211a533fa477656eb1363978aa1242c5
Jordan Fraughton <fraughtdawg@gmail.com>
2024-07-01 08:40:01 -0600

    Initial Commit

---
+++ TechSocialMediaSwiftUI/TechSocialMediaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechSocialMediaSwiftUIApp.swift
+//  TechSocialMediaSwiftUI
+//
+//  Created by Jordan Fraughton on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TechSocialMediaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
